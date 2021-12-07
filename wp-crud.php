4ce0164032822adabab5efbbe0b06349336cb966
guidoriosciaffaroni <guidoriosciaffaroni@gmail.com>
2021-12-07 08:41:11 -0300

    2021_12_07_08_41_11

--- wp-crud.php
+++ wp-crud.php
@@ -20,14 +20,16 @@ defined( 'ABSPATH' ) or die( '¡Sin trampas!' );
 require_once plugin_dir_path( __FILE__ ) . 'includes/install.php';
 
 
-/*Funciones requeridas para subir archivos */
-/* https://wordpress.stackexchange.com/questions/251236/upload-images-to-custom-database-table-in-admin-backend */
- require_once(ABSPATH . "wp-admin" . '/includes/image.php');
- require_once(ABSPATH . "wp-admin" . '/includes/file.php');
- require_once(ABSPATH . "wp-admin" . '/includes/media.php');
+/*Funciones requeridas para gestionar archivos */
+require_once(ABSPATH . "wp-admin" . '/includes/image.php');
+require_once(ABSPATH . "wp-admin" . '/includes/file.php');
+require_once(ABSPATH . "wp-admin" . '/includes/media.php');
+
+/*Funciones requeridas para gestionar la base de datos */
+require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 
 
-
+/*Variables globales*/
 global $wpbc_db_version;
 $wpbc_db_version = '1.1.0'; 
 
@@ -35,6 +37,22 @@ global $sistname;
 $sistname = 'crud'; 
 
 
+
+
+    global $wpdb;
+    global $wpbc_db_version;
+
+    $table_name = $wpdb->prefix . $sistname; 
+ 
+    $sql = "CREATE TABLE " . $table_name . " (
+        id int(11) NOT NULL AUTO_INCREMENT,
+        nint VARCHAR (50) NOT NULL, 
+        dir_archivo_externo VARCHAR (100) NOT NULL,
+        create_at datetime NOT NULL DEFAULT NOW(),
+        PRIMARY KEY  (id)
+    );";
+
+    dbDelta($sql);
 
 
 
