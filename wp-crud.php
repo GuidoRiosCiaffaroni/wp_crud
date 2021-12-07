7bbcffd0a9517c2fee9d504491871d22b95d165f
guidoriosciaffaroni <guidoriosciaffaroni@gmail.com>
2021-12-06 22:40:32 -0300

    2021_12_06_22_40_31

---
+++ wp-crud.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Plugin Name: WP CRUD
+* Description: Ejemplo Basico 
+* Version:     1.0
+* Plugin URI: https://guidorios.cl/wp-basic-crud-plugin-wordpress/
+* Author:      Guido Rios Ciaffaroni
+* Author URI:  https://guidorios.cl/
+* License:     GPLv2 or later
+* License URI: https://www.gnu.org/licenses/gpl-2.0.html
+* Text Domain: wpbc
+* Domain Path: /languages
+*/
+
+
+
+?>
+
