042c90768399de99f08268873db8421fe5be1b4d
guidoriosciaffaroni <guidoriosciaffaroni@gmail.com>
2021-12-10 15:36:12 -0300

    2021_12_10_15_36_12

--- includes/frontend_delete.php
+++ includes/frontend_delete.php
@@ -35,71 +35,46 @@ function Kfp_Delete_form()
     // nombre de los campos de la tabla
     foreach ($registros as $registros) 
     {
-        $result .= 
-            '
-            <tr>
-                <th>'.$registros->id.'</th>
-                <th>'.$registros->user_id.'</th> 
-                <th>'.$registros->key_id.'</th> 
-                <th>'.$registros->nint.'</th>
-                <th>'.$registros->date.'</th>
-                <th>'.$registros->dir_file_linux.'</th>
-                <th>'.$registros->dir_file_win.'</th>
-                <th>'.$registros->dir_file.'</th>
-                <th>'.$registros->status_id.'</th>
-                <th>'.$registros->create_at.'</th>                 
-            </tr>
-            ';
-            $key_id = $registros->key_id; 
-/*
             $id = $registros->id;
             $user_id = $registros->user_id; 
             $key_id = $registros->key_id; 
             $nint = $registros->nint;
             $date = $registros->date;
             $dir_file_linux = $registros->dir_file_linux;
-            $registros->dir_file_win;
-            $registros->dir_file;
-            $registros->status_id;
-            $registros->create_at;  
-
-*/
-
-        }
-
-        $template = '<table class="table-data">
-                      <tr>
-                        <th></th>
-                        <th></th>
-                        <th></th> 
-                        <th></th> 
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                      </tr>
-                      {data}
-                    </table>';
-
-        return $content.str_replace('{data}', $result, $template);        
+            $dir_file_win = $registros->dir_file_win;
+            $dir_file = $registros->dir_file;
+            $status_id = $registros->status_id;
+            $create_at = $registros->create_at;  
+    }
 
 
-
-        // $query = 'SELECT * FROM wp_crud WHERE id = '.$id;
-        //$query = 'UPDATE wp_crud SET status_id = 0 WHERE ( key_id = '.$registros->key_id.')';
-
-
-
-        $wpdb->update( 'wp_crud', 
-            // Datos que se remplazarán
-            array( 
-                'status_id' => '0'
+    $wpdb->update( 'wp_crud', 
+        // Datos que se remplazarán
+        array( 
+            'status_id' => '0'
             ),
             // Cuando el ID del campo es igual al número 1
             array( 'key_id' => $key_id )
         );
+
+
+    $wpdb->insert(
+            $tabla_crud,
+            array(
+                'user_id' => $user_id,
+                'key_id' => $key_id,
+                'nint'  => $nint,
+                'date'  => $date,
+                'dir_file_linux'  => $dir_file_linux,
+                'dir_file_win'  => $dir_file_win,
+                'dir_file'  => $dir_file,
+                'status_id' => 0,
+            )
+        );
+
+
+
+
 
 
 // header("Location: http://localhost/wordpress/insert/");
